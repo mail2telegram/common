808b010454568e0b1dc83f9f019eb0b8c2a42431
Alexey Shebarshin <madonnovna@gmail.com>
2020-07-05 15:28:24 +0500

    Add $draftEmail property and rename $process

--- src/Model/Account.php
+++ src/Model/Account.php
@@ -2,19 +2,29 @@
 
 namespace M2T\Model;
 
+/**
+ * @property Email[] $emails
+ * @property DraftEmail|null $draftEmail
+ * */
 class Account
 {
-    /** @var Email[] */
     public array $emails;
     public int $chatId;
-    public ?string $step = null;
-    public ?string $process = null;
+    public ?string $step;
+    public ?string $strategy;
+    public ?DraftEmail $draftEmail;
 
     public function __construct(
         int $chatId,
-        array $emails = []
+        array $emails = [],
+        ?string $step = null,
+        ?string $strategy = null,
+        ?string $draftEmail = null
     ) {
         $this->chatId = $chatId;
         $this->emails = $emails;
+        $this->step = $step;
+        $this->strategy = $strategy;
+        $this->draftEmail = $draftEmail;
     }
 }
