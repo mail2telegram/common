ab4dd92dd3cc94a9f8260113a5dd8ada0d767eb5
Alexey Shebarshin <madonnovna@gmail.com>
2020-07-05 15:27:37 +0500

    Add methods for get Account's emails information

--- src/AccountManager.php
+++ src/AccountManager.php
@@ -3,6 +3,7 @@
 namespace M2T;
 
 use M2T\Model\Account;
+use M2T\Model\Email;
 use Redis;
 
 class AccountManager
@@ -39,6 +40,51 @@ class AccountManager
     public function delete(int $chatId): bool
     {
         return 1 === $this->redis->del(static::getKey($chatId));
+    }
+
+    public function setAllEmailsNotSelected(Account $account): bool
+    {
+        foreach ($account->emails as $email){
+            $email->selected = false;
+        }
+        return $this->save($account);
+    }
+
+    public function checkExistEmail(Account $account, string $email): bool
+    {
+        foreach ($account->emails as $existEmail){
+            if($email == $existEmail->email) return true;
+        }
+        return false;
+    }
+
+    public function getSelectedEmail(Account $account): ?Email
+    {
+        foreach ($account->emails as $existEmail){
+            if($existEmail->selected) return $existEmail;
+        }
+        return null;
+    }
+
+    public function getEmail(Account $account, string $email): ?Email
+    {
+        foreach ($account->emails as $existEmail){
+            if($email == $existEmail->email) return $existEmail;
+        }
+        return null;
+    }
+
+    public function deleteEmail(Account $account, string $email): bool
+    {
+        foreach ($account->emails as $key => $existEmail){
+            if($email == $existEmail->email){
+                unset($account->emails[$key]);
+                $account->emails = array_values($account->emails);
+                $this->save($account);
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
