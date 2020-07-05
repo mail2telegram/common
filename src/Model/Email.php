07e66c296f8999c5b8558baa2dc0cd72236ef4f7
Alexey Shebarshin <madonnovna@gmail.com>
2020-07-05 15:29:15 +0500

    Update mailbox entity, add DraftEmail entity

--- src/Model/Email.php
+++ src/Model/Email.php
@@ -12,6 +12,7 @@ class Email
     public string $smtpHost;
     public int $smtpPort;
     public string $smtpSocketType;
+    public bool $selected;
 
     public function __construct(
         string $email,
@@ -21,7 +22,8 @@ class Email
         string $imapSocketType,
         string $smtpHost,
         string $smtpPort,
-        string $smtpSocketType
+        string $smtpSocketType,
+        bool $selected = false
     ) {
         $this->email = $email;
         $this->pwd = $pwd;
@@ -31,5 +33,22 @@ class Email
         $this->smtpHost = $smtpHost;
         $this->smtpPort = $smtpPort;
         $this->smtpSocketType = $smtpSocketType;
+        $this->selected = $selected;
     }
+
+
+    public function getSettings(){
+        return
+            '<strong>email:</strong> '.$this->email.PHP_EOL.
+            '<strong>password:</strong> ***'.PHP_EOL.
+            //'<strong>password:</strong> '.$this->pwd.PHP_EOL.
+            '<strong>imapHost:</strong> '.$this->imapHost.PHP_EOL.
+            '<strong>imapPort:</strong> '.$this->imapPort.PHP_EOL.
+            '<strong>imapSocketType:</strong> '.$this->imapSocketType.PHP_EOL.
+            '<strong>smtpHost:</strong> '.$this->smtpHost.PHP_EOL.
+            '<strong>smtpPort:</strong> '.$this->smtpPort.PHP_EOL.
+            '<strong>smtpSocketType:</strong> '.$this->smtpSocketType
+            ;
+    }
+
 }
